06423c9f4516f0389b50bc8b150de640d0fcba70
Glener Diniz Macedo <gdmacedo@hotmail.com>
2023-10-12 21:14:25 -0300

    Create index.js
    Lançamento inicial

---
+++ index.js
@@ -0,0 +1,73 @@
+/*
+-----------------------------------------------------------------
+Author..: Macedo, G. Diniz - Desenvolvedor Full Stack
+Projeto.: Classe generica que represente um herói de uma 
+          aventura e que possua as seguintes propriedades
+Fonte...: DIO.me - Digital Innovation One
+Objetivo: Usar os conhecimento em estruturas condicionais e 
+          funções para criar um algoritmo decisivo.
+Docente.: Felipe Aguiar - Tech Educator
+-----------------------------------------------------------------
+
+## Objetivo:
+
+classe generica que represente um herói de uma aventura e que 
+possua as seguintes propriedades nome, idade e tipo (ex: guerreiro, mago, monge, ninja )
+
+Além disso, deve ter um método chamado atacar que deve atender os seguientes requisitos:
+
+* exibir a mensagem: "o {tipo} atacou usando {ataque}")
+* aonde o {tipo} deve ser concatenando o tipo que está na propriedade da classe
+* e no {ataque} deve seguir uma descrição diferente conforme o tipo, seguindo a tabela abaixo:
+
+- Se mago -> no ataque exibir (usou magia)
+- se guerreiro -> no ataque exibir (usou espada)
+- se monge -> no ataque exibir (usou artes marciais)
+- se ninja -> no ataque exibir (usou shuriken)
+
+## Saída
+Ao final deve se exibir uma mensagem:
+
+- "o {tipo} atacou usando {ataque}" ex: "mago atacou usando magia", ou "guerreiro atacou usando espada"
+
+*/
+
+
+class clss_Heroi {
+    constructor(cNome, nIdade, cTipo) {
+		this.nome = cNome;
+		this.idade = nIdade;
+		this.tipo = cTipo;
+	}
+  
+    fnct_Atacar() {
+      let cAtaque = "";
+      switch (this.tipo) {
+        case "mago":
+          cAtaque = "usou magia";
+          break;
+        case "guerreiro":
+          cAtaque = "usou espada";
+          break;
+        case "monge":
+          cAtaque = "usou artes marciais";
+          break;
+        case "ninja":
+          cAtaque = "usou shuriken";
+          break;
+      }
+      console.log(`O ${this.tipo} atacou usando ${cAtaque}`);
+    }
+  }
+  
+  let mago = new clss_Heroi("Gandalf", 100, "mago");
+  let guerreiro = new clss_Heroi("Aragorn", 30, "guerreiro");
+  let monge = new clss_Heroi("Bruce Lee", 40, "monge");
+  let ninja = new clss_Heroi("Naruto", 20, "ninja");
+  
+  mago.fnct_Atacar()
+  guerreiro.fnct_Atacar()
+  monge.fnct_Atacar()
+  ninja.fnct_Atacar()
+  
+  
